d2e4e365e639379f8bc0092a6c16eb9f26caa2c9
rudgh420 <abc40abc40@naver.com>
2022-10-18 23:14:36 +0900

    new file

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print 'hello world'
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> print(hello world)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print('hello world')
+hello world
